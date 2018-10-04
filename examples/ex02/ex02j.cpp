b666a45e747e2a092b7910dee57d29038b6003a8
Lilin Yang <lyangl@gmail.com>
2018-10-05 00:00:47 +0800

    ex02j and ex02k

---
+++ examples/ex02/ex02j.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number_of_words{0};
+  string previous{" "};
+  string current;
+  while (cin >> current) {
+    ++number_of_words;
+    if (previous == current) {
+      cout << "word number " << number_of_words << " repeated: ";
+      cout << current << '\n';
+    }
+    previous = current;
+  }
+}
---
+++ examples/ex02/ex02k.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double d{0};
+  while (cin >> d) {
+    int i = d;
+    char c = i;
+    int i2 = c;
+    cout << "d==" << d;
+    cout << " i==" << i;
+    cout << " i2==" << i2;
+    cout << " char(" << c << ")\n";
+  }
+}
