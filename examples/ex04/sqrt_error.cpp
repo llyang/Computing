b06ea5e8bd02459fbe325d01086697b48708e752
Lilin Yang <lyangl@gmail.com>
2018-10-15 16:16:48 +0800

    sqrt

---
+++ examples/ex04/sqrt_error.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double my_sqrt(double x) {
+
+  const double error{1e-6};
+  double a{x};
+  double b{1};
+  while (abs((a - b) / min(a, b)) > error) {
+    a = (a + b) / 2;
+    b = x / a;
+  }
+  return a;
+}
+
+int main() {
+  double x;
+  cin >> x;
+  cout << my_sqrt(x) << '\n';
+}
---
+++ examples/ex04/sqrt_nan.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double my_sqrt(double x) {
+  if (x < 0) {
+    return nan("my_sqrt");
+  }
+
+  const double error{1e-6};
+  double a{x};
+  double b{1};
+  while (abs((a - b) / min(a, b)) > error) {
+    a = (a + b) / 2;
+    b = x / a;
+  }
+  return a;
+}
+
+int main() {
+  double x;
+  cin >> x;
+  double y = my_sqrt(x);
+  if (isnan(y)) {
+    cout << "my_sqrt error!\n";
+    return 1;
+  }
+  cout << y << '\n';
+}
+
+
