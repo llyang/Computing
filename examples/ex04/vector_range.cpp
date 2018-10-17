f815d5dd59ff31fb9fabbd4b3b0bde7d18d9d0e1
Lilin Yang <lyangl@gmail.com>
2018-10-17 14:13:54 +0800

    vector_range

---
+++ examples/ex04/vector_range.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> v{1, 2, 3, 4, 5};
+  int i;
+  cin >> i;
+  try {
+    int n = v.at(i);
+    cout << "v[" << i << "] = " << n << '\n';
+  } catch (out_of_range) {
+    cout << "out of range: i = " << i << '\n';
+  }
+}
+
