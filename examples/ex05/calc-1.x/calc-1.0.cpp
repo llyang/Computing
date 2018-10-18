26d8051de84959606914abb7688e6e2c9db6092c
Lilin Yang <lyangl@gmail.com>
2018-10-18 14:01:47 +0800

    calc-1.x

---
+++ examples/ex05/calc-1.x/calc-1.0.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Expression: ";
+
+  double lval{0};
+  double rval{0};
+  char op{' '};
+  cin >> lval >> op >> rval;
+  if (!cin) {
+    throw runtime_error("input error");
+  }
+
+  double res;
+  switch (op) {
+  case '+':
+    res = lval + rval;
+    break;
+  case '-':
+    res = lval - rval;
+    break;
+  case '*':
+    res = lval * rval;
+    break;
+  case '/':
+    res = lval / rval;
+    break;
+  default:
+    throw runtime_error("unknown operator");
+  }
+
+  cout << "Result: " << res << '\n';
+
+  return 0;
+}
---
+++ examples/ex05/calc-1.x/calc-1.1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+  cout << "Expression (end with ';'): ";
+
+  double lval{0};
+  cin >> lval;
+  if (!cin) {
+    throw runtime_error("no first operand");
+  }
+
+  for (char op; cin >> op;) {
+    double rval{0};
+    if (op != ';') {
+      cin >> rval;
+    }
+    if (!cin) {
+      throw runtime_error("no second operand");
+    }
+    switch (op) {
+    case '+':
+      lval += rval;
+      break;
+    case '-':
+      lval -= rval;
+      break;
+    case '*':
+      lval *= rval;
+      break;
+    case '/':
+      lval /= rval;
+      break;
+    default:
+      cout << "Result: " << lval << '\n';
+      return 0;
+    }
+  }
+}
