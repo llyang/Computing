2589345fe6b33e5d2875699f71c3973dfce002b4
Lilin Yang <lyangl@gmail.com>
2018-10-31 17:16:20 +0800

    calc-2.3

---
+++ examples/ex06/calc-2.3/Token.h
@@ -0,0 +1,11 @@
+#ifndef _TOKEN_H
+#define _TOKEN_H
+
+class Token {
+public:
+  char kind;
+  double value;
+};
+
+#endif
+
---
+++ examples/ex06/calc-2.3/calc-2.3.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "Token.h"
+#include "Token_stream.h"
+
+// A global variable, not preferred, just for now
+Token_stream ts;
+
+double expression();
+double term();
+double primary();
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::exception;
+using std::runtime_error;
+
+int main()
+{
+  try {
+    double val { 0 };
+    cout << "> ";
+    while (cin) {
+      Token t { ts.get() };
+      if (t.kind == 'q') { // quit
+        break;
+      }
+      if (t.kind == ';') { // end of an expression
+        cout << "= " << val << '\n';
+        cout << "> ";
+      } else {
+        ts.putback(t);
+      }
+      val = expression();
+    }
+  } catch (exception& e) {
+    cerr << e.what() << '\n';
+    return 1;
+  } catch (...) {
+    cerr << "unknown error\n";
+    return 2;
+  }
+}
+
+// return the value of the next expression
+double expression()
+{
+  double left { term() };
+  while (true) {
+    Token t { ts.get() };
+    switch (t.kind) {
+    case '+':
+      left += term();
+      break;
+    case '-':
+      left -= term();
+      break;
+    default:
+      ts.putback(t);
+      return left;
+    }
+  }
+}
+
+// return the value of the next term
+double term()
+{
+  double left { primary() };
+  while (true) {
+    Token t { ts.get() };
+    switch (t.kind) {
+    case '*':
+      left *= primary();
+      break;
+    case '/': {
+      double d { primary() };
+      if (d == 0) {
+        throw runtime_error { "divide by zero" };
+      }
+      left /= d;
+      break;
+    }
+    default:
+      ts.putback(t);
+      return left;
+    }
+  }
+}
+
+// return the value of the next primary
+double primary()
+{
+  Token t { ts.get() };
+  switch (t.kind) {
+  case '(': {
+    double d { expression() };
+    t = ts.get();
+    if (t.kind != ')') {
+      throw runtime_error { "')' expected" };
+    }
+    return d;
+  }
+  case '6':
+    return t.value;
+  default:
+    throw runtime_error { "primary expected" };
+  }
+}
+
