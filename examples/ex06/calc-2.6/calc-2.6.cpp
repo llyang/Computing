42c77239aa217297470d963f1221879cef2e2011
Lilin Yang <lyangl@gmail.com>
2018-11-01 22:56:36 +0800

    calc-2.6

---
+++ examples/ex06/calc-2.6/calc-2.6.cpp
@@ -0,0 +1,144 @@
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+#include "Token.h"
+#include "Token_stream.h"
+
+// A global variable, not preferred, just for now
+Token_stream ts;
+
+void calculate();
+void clean_up_mess();
+double expression();
+double term();
+double primary();
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::exception;
+using std::runtime_error;
+using std::string;
+
+int main()
+{
+  try {
+    calculate();
+    return 0;
+  } catch (...) {
+    cerr << "unknown error\n";
+    return 2;
+  }
+}
+
+void clean_up_mess()
+{
+  while (true) {
+    Token t { ts.get() };
+    if (t.kind == print) {
+      return;
+    }
+  }
+}
+
+void calculate()
+{
+  const string prompt { "> " };
+  const string result { "= " };
+  while (cin) {
+    try {
+      cout << prompt;
+      Token t { ts.get() };
+      while (t.kind == print) {
+        t = ts.get();
+      }
+      if (t.kind == quit) { // quit
+        return;
+      }
+      ts.putback(t);
+      cout << result << expression() << '\n';
+    } catch (exception& e) {
+      cerr << e.what() << '\n';
+      clean_up_mess();
+    }
+  }
+}
+
+// return the value of the next expression
+double expression()
+{
+  double left { term() };
+  while (true) {
+    Token t { ts.get() };
+    switch (t.kind) {
+    case '+':
+      left += term();
+      break;
+    case '-':
+      left -= term();
+      break;
+    default:
+      ts.putback(t);
+      return left;
+    }
+  }
+}
+
+// return the value of the next term
+double term()
+{
+  double left { primary() };
+  while (true) {
+    Token t { ts.get() };
+    switch (t.kind) {
+    case '*':
+      left *= primary();
+      break;
+    case '/': {
+      double d { primary() };
+      if (d == 0) {
+        throw runtime_error { "divide by zero" };
+      }
+      left /= d;
+      break;
+    }
+    case '%': {
+      double d { primary() };
+      if (d == 0) {
+        throw runtime_error { "divide by zero" };
+      }
+      left = std::fmod(left, d);
+      break;
+    }
+    default:
+      ts.putback(t);
+      return left;
+    }
+  }
+}
+
+// return the value of the next primary
+double primary()
+{
+  Token t { ts.get() };
+  switch (t.kind) {
+  case '(': {
+    double d { expression() };
+    t = ts.get();
+    if (t.kind != ')') {
+      throw runtime_error { "')' expected" };
+    }
+    return d;
+  }
+  case number:
+    return t.value;
+  case '-':
+    return -primary();
+  case '+':
+    return primary();
+  default:
+    throw runtime_error { "primary expected" };
+  }
+}
+
