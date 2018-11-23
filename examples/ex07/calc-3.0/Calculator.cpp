ab88d44c464b23116e728449c1e448ccf33afb52
Lilin Yang <lyangl@gmail.com>
2018-11-23 22:11:06 +0800

    ex07

--- examples/ex07/calc-3.0/Calculator.cpp
+++ examples/ex07/calc-3.0/Calculator.cpp
@@ -54,9 +54,9 @@ double declaration(Token_stream& ts)
   }
   string var_name { t.name };
 
-  Token t2 { ts.get() };
-  if (t2.kind != '=') {
-    ts.putback(t2);
+  t = ts.get();
+  if (t.kind != '=') {
+    ts.putback(t);
     throw runtime_error { "= missing in declaration of " + var_name };
   }
 
@@ -68,13 +68,19 @@ double declaration(Token_stream& ts)
 double statement(Token_stream& ts)
 {
   Token t { ts.get() };
-  switch (t.kind) {
-  case let:
-    return declaration(ts);
-  default:
+  double r { 0 };
+  if (t.kind == let) {
+    r = declaration(ts);
+  } else {
     ts.putback(t);
-    return expression(ts);
+    r = expression(ts);
   }
+
+  t = ts.get();
+  if (t.kind != print)
+    throw runtime_error { "illegal statement" };
+
+  return r;
 }
 
 // return the value of the next expression
