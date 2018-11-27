b900541c6c4d582886b43fc89a928bfbed01e671
Lilin Yang <lyangl@gmail.com>
2018-11-27 08:12:04 +0800

    test_scope

---
+++ examples/ex07/misc/test_scope.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using std::cout;
+
+int x { 1 };
+
+namespace haha {
+int x { 2 };
+}
+
+class A {
+public:
+  void f()
+  {
+    cout << "global x: " << ::x << '\n';
+    cout << "x in namespace haha: " << haha::x << '\n';
+    cout << "x in class A: " << x << '\n';
+    int x { 4 };
+    cout << "x in function f(): " << x << '\n';
+    for (int x = 5; x < 6; ++x) {
+      cout << "x in the for statement: " << x << '\n';
+      {
+        int x { 6 };
+        cout << "x in the curly braket: " << x << '\n';
+        cout << "global x: " << ::x << '\n';
+        cout << "x in namespace haha: " << haha::x << '\n';
+        cout << "x in class A: " << this->x << '\n';
+      }
+    }
+  }
+
+private:
+  int x { 3 };
+};
+
+int main()
+{
+  A a;
+  a.f();
+}
